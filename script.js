009b2fa303c3a8ac2858b7809a8b4e054baca8a3
Syed Amash S <syedamashtce@gmail.com>
2025-03-19 11:21:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,15 @@ const fetchPrice = async () => {
         updatePrice("kadena", data.kadena);
         
 
-
+        if (data.ethereum) updatePrice("ethereum", data.ethereum);
+        if (data.solana) updatePrice("solana", data.solana);
+        if (data.kadena) updatePrice("kadena", data.kadena);
+        if (data.aptos) updatePrice("aptos", data.aptos);
+        if (data.cardano) updatePrice("cardano", data.cardano);
+        if (data.sui) updatePrice("sui", data.sui);
+        if (data.dogecoin) updatePrice("dogecoin", data.dogecoin);
+        if (data.binancecoin) updatePrice("binancecoin", data.binancecoin);
+        
         // Hide loading spinner and stop refresh button animation
         document.getElementById("loadingSpinner").style.display = "none";
         document.getElementById("refreshButton").classList.remove("refreshing");
@@ -60,7 +68,6 @@ const fetchPrice = async () => {
         console.log("Error fetching prices: ", error);
         document.getElementById("loadingSpinner").style.display = "none";
         document.getElementById("refreshButton").classList.remove("refreshing");
-        alert("Failed to fetch prices. Please try again later.");
     }
 };
 
